6853973affdaad6fc9378c643df1f04d25dece52
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2021-02-07 22:50:00 +0900

    History API類

--- content/docs/front-end/browser_api.md
+++ content/docs/front-end/browser_api.md
@@ -11,3 +11,47 @@ weight: 1
 # ブラウザ関連API
 
 ブラウザに関するAPIについてを述べる。
+
+# History API
+
+History APIは、ブラウザの履歴を提供するためのAPIである。また、bブラウザの戻る・進むボタンの様なページを遷移するためのイベントもここで行える。
+
+主なプロパティ及びメソッドは以下の通り。
+
+<table style="border:none;">
+    <tr>
+        <th style="border:none;">プロパティ名</td>
+        <th style="border:none;">意味</td>
+    </tr>
+    <tr>
+        <td style="border:none;">length</td>
+        <td style="border:none;">履歴の数</td>
+    </tr>
+    <tr>
+        <td style="border:none;">state</td>
+        <td style="border:none;">最後に設定された状態</td>
+    </tr>
+</table>
+
+<table style="border:none;">
+    <tr>
+        <th style="border:none;">メソッド</td>
+        <th style="border:none;">意味</td>
+    </tr>
+    <tr>
+        <td style="border:none;">go()</td>
+        <td style="border:none;">履歴において指定した番目のページへ遷移する</td>
+    </tr>
+    <tr>
+        <td style="border:none;">back()</td>
+        <td style="border:none;">1つ前のページへ遷移する</td>
+    </tr>
+    <tr>
+        <td style="border:none;">forward()</td>
+        <td style="border:none;">1つ後のページへ遷移する</td>
+    </tr>
+</table>
+
+これらをJavascript上で利用することで、ページの遷移も行えることができる。
+
+
--- docs/en.search.min.71dd8bd0f37ac2ee9e3cca0287d10bf1bce689648465777735bf4b5ac1983c05.js
+++ docs/en.search.min.71dd8bd0f37ac2ee9e3cca0287d10bf1bce689648465777735bf4b5ac1983c05.js
@@ -3,7 +3,7 @@ input.addEventListener('focus',init);input.addEventListener('keyup',search);docu
 const characterPressed=String.fromCharCode(event.charCode);if(!isHotkey(characterPressed)){return;}
 input.focus();event.preventDefault();}
 function isHotkey(character){const dataHotkeys=input.getAttribute('data-hotkeys')||'';return dataHotkeys.indexOf(character)>=0;}
-function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/flexsearch.min.js');loadScript('/en.search-data.min.7e76ab85bb3f0c133e6c97a5c464c71be57cbc8cb41633235241ac8c18e70f31.js',function(){input.required=false;search();});}
+function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/flexsearch.min.js');loadScript('/en.search-data.min.ab188c1a0588945c410c4608131b9ab51af817c98efcadaee1bd4656e5a4889a.js',function(){input.required=false;search();});}
 function search(){while(results.firstChild){results.removeChild(results.firstChild);}
 if(!input.value){return;}
 const searchHits=window.bookSearchIndex.search(input.value,10);searchHits.forEach(function(page){const li=element('<li><a href></a><small></small></li>');const a=li.querySelector('a'),small=li.querySelector('small');a.href=page.href;a.textContent=page.title;small.textContent=page.section;results.appendChild(li);});}
