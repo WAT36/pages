2d35adf6e79475a71dd53bd8bcc867a761cf35c4
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2021-02-07 22:19:58 +0900

    セレクタAPI　変えたの少しだけ

--- docs/en.search.min.bf977299820fc3e75a9332e0686384379e79c463fe25aa5ab6c4b9b8820d8d37.js
+++ docs/en.search.min.bf977299820fc3e75a9332e0686384379e79c463fe25aa5ab6c4b9b8820d8d37.js
@@ -3,7 +3,7 @@ input.addEventListener('focus',init);input.addEventListener('keyup',search);docu
 const characterPressed=String.fromCharCode(event.charCode);if(!isHotkey(characterPressed)){return;}
 input.focus();event.preventDefault();}
 function isHotkey(character){const dataHotkeys=input.getAttribute('data-hotkeys')||'';return dataHotkeys.indexOf(character)>=0;}
-function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/flexsearch.min.js');loadScript('/en.search-data.min.7b4f5f39d85e2b19e28f2d2a57b44e37356f8695a5923ccc34356985910efc92.js',function(){input.required=false;search();});}
+function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/flexsearch.min.js');loadScript('/en.search-data.min.4c127c759e5b43200c879922b1f52a11db927482c2d8dca2b73ac7c94b39e4f2.js',function(){input.required=false;search();});}
 function search(){while(results.firstChild){results.removeChild(results.firstChild);}
 if(!input.value){return;}
 const searchHits=window.bookSearchIndex.search(input.value,10);searchHits.forEach(function(page){const li=element('<li><a href></a><small></small></li>');const a=li.querySelector('a'),small=li.querySelector('small');a.href=page.href;a.textContent=page.title;small.textContent=page.section;results.appendChild(li);});}
