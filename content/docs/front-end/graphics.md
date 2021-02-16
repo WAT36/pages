f9bdb0036774e570110ea397d02ec9168ba78147
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2021-02-16 23:22:43 +0900

    グラフィックスページ追加

---
+++ content/docs/front-end/graphics.md
@@ -0,0 +1,12 @@
+---
+title: "グラフィックス・アニメーション"
+weight: 1
+# bookFlatSection: false
+# bookToc: true
+# bookHidden: false
+# bookCollapseSection: false
+# bookComments: true
+---
+
+# グラフィックス・アニメーション
+
--- docs/en.search.min.16575ee470b8bc08c11f8330b204b51dcab17c697971324a1fb2c238419e3f2e.js
+++ docs/en.search.min.16575ee470b8bc08c11f8330b204b51dcab17c697971324a1fb2c238419e3f2e.js
@@ -3,7 +3,7 @@ input.addEventListener('focus',init);input.addEventListener('keyup',search);docu
 const characterPressed=String.fromCharCode(event.charCode);if(!isHotkey(characterPressed)){return;}
 input.focus();event.preventDefault();}
 function isHotkey(character){const dataHotkeys=input.getAttribute('data-hotkeys')||'';return dataHotkeys.indexOf(character)>=0;}
-function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/flexsearch.min.js');loadScript('/en.search-data.min.cdc36816876b168968bbf27e18620cc81c0ccc3259c5b229bbd34ae889d85076.js',function(){input.required=false;search();});}
+function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/flexsearch.min.js');loadScript('/en.search-data.min.c4bae59fcdb17cdde38b05a23d12c3f21becddd4b06e15b5938563bc64a460d6.js',function(){input.required=false;search();});}
 function search(){while(results.firstChild){results.removeChild(results.firstChild);}
 if(!input.value){return;}
 const searchHits=window.bookSearchIndex.search(input.value,10);searchHits.forEach(function(page){const li=element('<li><a href></a><small></small></li>');const a=li.querySelector('a'),small=li.querySelector('small');a.href=page.href;a.textContent=page.title;small.textContent=page.section;results.appendChild(li);});}
