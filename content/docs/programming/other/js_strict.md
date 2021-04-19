5560fbeba778aeaac2ba6dd1ad68c6402114653a
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2021-04-19 22:01:53 +0900

    少し修正

--- content/docs/programming/other/js_strict.md
+++ content/docs/programming/other/js_strict.md
@@ -8,13 +8,15 @@ weight: 1
 # bookComments: true
 ---
 
+# Strictモード(Javascript)
+
 JavascriptのStrictモードについて。
 
 Javascriptの仕様には様々な理由で曖昧さが存在しており、それが時にエラーの原因になりうることもある。
 
-これらの問題の原因に対して、厳格な(Strict)チェックをするための昨日が**Strictモード**になる。
+これらの問題の原因に対して、厳格な(Strict)チェックをするための機能が**Strictモード**になる。
 
-このStrictモードを指定することにより、エラーを発見が容易になったり、適切な最適化が行え処理を高速化することができる。
+このStrictモードを指定することにより、エラーの発見が容易になったり、適切な最適化が行え処理を高速化することができる。
 
 Strictモードにするには、以下の宣言を記述する。
 
@@ -24,3 +26,40 @@ Strictモードにするには、以下の宣言を記述する。
 ```
 
 記述した箇所によって適用されるスコープが変わるので注意。ソースコードの先頭に記述した場合はソースコード全体に、関数内の先頭に記述した場合はその関数全体に適用される。
+
+
+# 曖昧さとは
+
+曖昧さとは、例えばJavascriptの予約語を変数名として利用してもエラーにならないなどといったようなものである。
+
+例えば、以下のjavascriptを実行してみると
+
+```javascript
+var package = 1;
+console.log(package);
+```
+
+ログには以下のように出力される。
+
+```
+1
+```
+
+通常、packageはjavascriptの予約語の一つであり、変数名として利用することはできない。
+
+ここで、Strictモードを適用してみると
+
+```javascript
+'use strict'
+
+var package = 1;
+console.log(package);
+```
+
+結果は以下のようになる。
+
+```
+SyntaxError: Cannot use the reserved word 'package' as a variable name in strict mode.
+```
+
+このように、Strictモードを使うことによって、曖昧な仕様に対する厳格なエラーチェックを行ってくれる。
--- docs/en.search.min.0c272f790c76819d1e775507299c35d2052773f62ba6cfbd9d0f5d66a280c7c1.js
+++ docs/en.search.min.0c272f790c76819d1e775507299c35d2052773f62ba6cfbd9d0f5d66a280c7c1.js
@@ -3,7 +3,7 @@ input.addEventListener('focus',init);input.addEventListener('keyup',search);docu
 const characterPressed=String.fromCharCode(event.charCode);if(!isHotkey(characterPressed)){return;}
 input.focus();event.preventDefault();}
 function isHotkey(character){const dataHotkeys=input.getAttribute('data-hotkeys')||'';return dataHotkeys.indexOf(character)>=0;}
-function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/flexsearch.min.js');loadScript('/en.search-data.min.5852b65f728a58224275e41bb8796b596db3232c4470dfc42c309062e81c973f.js',function(){input.required=false;search();});}
+function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/flexsearch.min.js');loadScript('/en.search-data.min.40433dd618b96d68fcf2447b36024bc71836170c09f771767285fc754ca6ba4e.js',function(){input.required=false;search();});}
 function search(){while(results.firstChild){results.removeChild(results.firstChild);}
 if(!input.value){return;}
 const searchHits=window.bookSearchIndex.search(input.value,10);searchHits.forEach(function(page){const li=element('<li><a href></a><small></small></li>');const a=li.querySelector('a'),small=li.querySelector('small');a.href=page.href;a.textContent=page.title;small.textContent=page.section;results.appendChild(li);});}
