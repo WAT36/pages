debebc8d094d89048a7fe3deb6c64464b1975932
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2021-05-21 21:53:03 +0900

    パフォーマンスAPIのページ追加

---
+++ content/docs/front-end/performance_js.md
@@ -0,0 +1,12 @@
+---
+title: "パフォーマンス系API(Javascript)"
+weight: 1
+# bookFlatSection: false
+# bookToc: true
+# bookHidden: false
+# bookCollapseSection: false
+# bookComments: true
+---
+
+# パフォーマンス系API(Javascript)
+
--- docs/en.search.min.ce1479f997bf37ee0894000b4c920c46d6a69f8f805fc5860b1ef8aff8387507.js
+++ docs/en.search.min.ce1479f997bf37ee0894000b4c920c46d6a69f8f805fc5860b1ef8aff8387507.js
@@ -3,7 +3,7 @@ input.addEventListener('focus',init);input.addEventListener('keyup',search);docu
 const characterPressed=String.fromCharCode(event.charCode);if(!isHotkey(characterPressed)){return;}
 input.focus();event.preventDefault();}
 function isHotkey(character){const dataHotkeys=input.getAttribute('data-hotkeys')||'';return dataHotkeys.indexOf(character)>=0;}
-function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/flexsearch.min.js');loadScript('/en.search-data.min.b46874bf46cd92083bcf0c07bed20ff4a5355ded82d31a217341bc004fd4a821.js',function(){input.required=false;search();});}
+function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/flexsearch.min.js');loadScript('/en.search-data.min.334681232ff3d69b03abdfb327a56ea6684a49f213e0a5a41708641442716812.js',function(){input.required=false;search();});}
 function search(){while(results.firstChild){results.removeChild(results.firstChild);}
 if(!input.value){return;}
 const searchHits=window.bookSearchIndex.search(input.value,10);searchHits.forEach(function(page){const li=element('<li><a href></a><small></small></li>');const a=li.querySelector('a'),small=li.querySelector('small');a.href=page.href;a.textContent=page.title;small.textContent=page.section;results.appendChild(li);});}
