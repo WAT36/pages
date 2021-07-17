a2e69a258712bbf4822f9665e9a9c6608f010502
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2021-07-17 17:26:24 +0900

    Infinity Javascript

--- content/docs/programming/number/infinity.md
+++ content/docs/programming/number/infinity.md
@@ -9,3 +9,58 @@ weight: 1
 ---
 
 # Infinity
+
+
+**Infinity**とは、全ての値より大きい値を示す値である。プログラムでは遥かに大きい値などを指定したい時んい利用することが多い。言語にもよるが計算で得られることもある。
+
+以下、各言語によるInfinityについてを記載する。
+
+{{< tabs "inf" >}}
+{{< tab "Java" >}}
+
+
+
+{{< /tab >}}
+{{< tab "Python" >}}
+
+
+
+{{< /tab >}}
+{{< tab "Javascript" >}}
+
+JavascriptではInfinityはグローバルオブジェクトのプロパティとして扱われており、普通にInfinityと宣言することで利用できる。通常では数値を0で除算するなどした時に(0/0はNaNになる)、Infinityが出力される。
+
+また、グローバルオブジェクトの**isFinite()**を使用すると、その値が有限の値であるかを判別できる。Infinity,NaNを入れた場合はfalseを返す。
+
+例（Infinity * 0はNaNになる）
+
+```javascript
+console.log(1/0)
+inf = 1/0
+console.log(inf == Infinity)
+console.log(isFinite(inf))
+console.log(isFinite(1))
+console.log(Infinity + 1)
+console.log(Infinity + Infinity)
+console.log(Infinity * -1)
+console.log(Infinity * 0)
+```
+
+実行結果
+
+```
+Infinity
+
+true
+false
+true
+Infinity
+Infinity
+-Infinity
+NaN
+```
+
+
+
+{{< /tab >}}
+{{< /tabs >}}
--- docs/en.search.min.834dba756957c55f20aa2698847c42d7b458d348e6fa6bf321ed61480538dd56.js
+++ docs/en.search.min.834dba756957c55f20aa2698847c42d7b458d348e6fa6bf321ed61480538dd56.js
@@ -3,7 +3,7 @@ input.addEventListener('focus',init);input.addEventListener('keyup',search);docu
 const characterPressed=String.fromCharCode(event.charCode);if(!isHotkey(characterPressed)){return;}
 input.focus();event.preventDefault();}
 function isHotkey(character){const dataHotkeys=input.getAttribute('data-hotkeys')||'';return dataHotkeys.indexOf(character)>=0;}
-function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/flexsearch.min.js');loadScript('/en.search-data.min.9077504e5435e6c0300b064dde56823f244834bdb15e935288f967555f70659e.js',function(){input.required=false;search();});}
+function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/flexsearch.min.js');loadScript('/en.search-data.min.c82bb638806f55792ca0ffd30008f8b2d44a26a86143fd2c35c84560bba9630f.js',function(){input.required=false;search();});}
 function search(){while(results.firstChild){results.removeChild(results.firstChild);}
 if(!input.value){return;}
 const searchHits=window.bookSearchIndex.search(input.value,10);searchHits.forEach(function(page){const li=element('<li><a href></a><small></small></li>');const a=li.querySelector('a'),small=li.querySelector('small');a.href=page.href;a.textContent=page.title;small.textContent=page.section;results.appendChild(li);});}
