c7a292600bec34f2e26d2ca2dd8cae3fc356f223
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2021-10-01 20:28:16 +0900

    Javascript　文字列の長さ

--- content/docs/programming/string/length.md
+++ content/docs/programming/string/length.md
@@ -63,4 +63,28 @@ Pythonでは引数の文字列の長さ（＝文字数）を返す関数**len()*
 
 
 {{< /tab >}}
+{{< tab "Javascript" >}}
+
+Javascriptでは文字列に長さ（＝文字数）を保持しているプロパティ**length**があるので、それを利用する。
+
+```javascript
+let a = "ab"
+let b = "11223344"
+let c = "**************"
+
+console.log(a.length)
+console.log(b.length)
+console.log(c.length)
+```
+
+実行結果
+
+```
+2
+8
+14
+```
+
+
+{{< /tab >}}
 {{< /tabs >}}
--- docs/en.search.min.7e1308c15bd897c98f92175781b1ff594e7c39dd7f8f553437f6ac48457f8913.js
+++ docs/en.search.min.7e1308c15bd897c98f92175781b1ff594e7c39dd7f8f553437f6ac48457f8913.js
@@ -3,7 +3,7 @@ input.addEventListener('focus',init);input.addEventListener('keyup',search);docu
 const characterPressed=String.fromCharCode(event.charCode);if(!isHotkey(characterPressed)){return;}
 input.focus();event.preventDefault();}
 function isHotkey(character){const dataHotkeys=input.getAttribute('data-hotkeys')||'';return dataHotkeys.indexOf(character)>=0;}
-function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/flexsearch.min.js');loadScript('/en.search-data.min.a0fcf752a750a22c5ab1525b5f0df205c78b385b38610a51156b4b1587faad99.js',function(){input.required=false;search();});}
+function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/flexsearch.min.js');loadScript('/en.search-data.min.a6988e218fa4985de4fa7169d26b25dc43eca10204fb1578e76ea9eab3a491a0.js',function(){input.required=false;search();});}
 function search(){while(results.firstChild){results.removeChild(results.firstChild);}
 if(!input.value){return;}
 const searchHits=window.bookSearchIndex.search(input.value,10);searchHits.forEach(function(page){const li=element('<li><a href></a><small></small></li>');const a=li.querySelector('a'),small=li.querySelector('small');a.href=page.href;a.textContent=page.title;small.textContent=page.section;results.appendChild(li);});}
