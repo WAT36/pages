ce6402a33506d579c1a52fb42b5a6b876561a306
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2021-10-23 00:12:48 +0900

    Javascript 数値ゼロ埋め方法

--- docs/en.search.min.0f688ea51a29372b4f546d74c58693f886f3a434a949eaa78e9cc9cff073b9b7.js
+++ docs/en.search.min.0f688ea51a29372b4f546d74c58693f886f3a434a949eaa78e9cc9cff073b9b7.js
@@ -3,7 +3,7 @@ input.addEventListener('focus',init);input.addEventListener('keyup',search);docu
 const characterPressed=String.fromCharCode(event.charCode);if(!isHotkey(characterPressed)){return;}
 input.focus();event.preventDefault();}
 function isHotkey(character){const dataHotkeys=input.getAttribute('data-hotkeys')||'';return dataHotkeys.indexOf(character)>=0;}
-function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/flexsearch.min.js');loadScript('/en.search-data.min.33c39db2f1991b447c95fdecd989052300e923d615832296e43395fd2053e4f5.js',function(){input.required=false;search();});}
+function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/flexsearch.min.js');loadScript('/en.search-data.min.8da7a1d39244806effc98b946b16dc55261cc375b979a5288fa84e82ef05b2e9.js',function(){input.required=false;search();});}
 function search(){while(results.firstChild){results.removeChild(results.firstChild);}
 if(!input.value){return;}
 const searchHits=window.bookSearchIndex.search(input.value,10);searchHits.forEach(function(page){const li=element('<li><a href></a><small></small></li>');const a=li.querySelector('a'),small=li.querySelector('small');a.href=page.href;a.textContent=page.title;small.textContent=page.section;results.appendChild(li);});}
